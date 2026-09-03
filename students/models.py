c49230c8304aa542d38f2641a042ebebb6228aa4
Kommu Abhishek <kommuabhishek84@gmail.com>
2026-09-03 14:40:57 +0530

    models,0001_initial.py

--- students/models.py
+++ students/models.py
@@ -1,9 +1,11 @@
+from datetime import timezone
+
 from django.db import models
 class Department(models.Model):
     name = models.CharField(max_length=100,unique=True)
     code = models.CharField(max_length=10, unique=True)
     hod_name = models.CharField(max_length=100, blank=True)
-   
+
 class Course(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=15, unique=True)
@@ -12,6 +14,24 @@ class Course(models.Model):
 
     semester = models.PositiveSmallIntegerField(default=1)
     credits = models.PositiveSmallIntegerField(default=1)
-   
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+
+class Student(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+    roll_no = models.CharField(max_length=20, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True)
+    data_of_birth = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='M')
+    department = models.ForeignKey(Department, on_delete=models.PROTECT, related_name='students',)
+    year_of_admission = models.PositiveSmallIntegerField(default=1)
+    address = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='student_photos/', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    admitted_no = models.DateField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
