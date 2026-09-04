8f54b7724af0b4f6b65d28a67f1bc52ff789755f
Kommu Abhishek <kommuabhishek84@gmail.com>
2026-09-04 13:44:25 +0530

    admin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Department, Course, Student
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Student)
